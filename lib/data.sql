2ac24d9b990f003c4b2bd5bce1dcc92a0e2e7097
Teresa <teriv1707@gmail.com>
2023-02-03 15:35:23 +0100

    mettez la base de donnees

--- lib/data.sql
+++ lib/data.sql
@@ -34,7 +34,7 @@ CREATE TABLE `adresse` (
   PRIMARY KEY (`id`),
   KEY `fk_adresse_user_idx` (`employee_id`),
   CONSTRAINT `fk_adresse_user` FOREIGN KEY (`employee_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,8 +43,48 @@ CREATE TABLE `adresse` (
 
 LOCK TABLES `adresse` WRITE;
 /*!40000 ALTER TABLE `adresse` DISABLE KEYS */;
-INSERT INTO `adresse` VALUES (1,'cvxc','xvxcv','vv','vv',1),(2,'14','rue du bac','carriere sous poissy','78955',10),(3,'88','makota','ain benian','70000',10);
+INSERT INTO `adresse` VALUES (6,'14','paris','Paris','78955',12),(7,'88','lyon','Lyon','70000',12);
 /*!40000 ALTER TABLE `adresse` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `salaire`
+--
+
+DROP TABLE IF EXISTS `salaire`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `salaire` (
+  `idSalaire` int NOT NULL AUTO_INCREMENT,
+  `ANNEE` int NOT NULL,
+  `JANVIER` float DEFAULT '0',
+  `FEVRIER` float DEFAULT '0',
+  `MARS` float DEFAULT '0',
+  `AVRIL` float DEFAULT '0',
+  `MAI` float DEFAULT '0',
+  `JUIN` float DEFAULT '0',
+  `JUILLET` float DEFAULT '0',
+  `AOUT` float DEFAULT '0',
+  `SEPTEMBRE` float DEFAULT '0',
+  `OCTOBRE` float DEFAULT '0',
+  `NOVEMBRE` float DEFAULT '0',
+  `DECEMBRE` float DEFAULT '0',
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`idSalaire`),
+  UNIQUE KEY `UQ_salaire_annee_user_id` (`ANNEE`,`user_id`),
+  KEY `fk_Salaire_user1_idx` (`user_id`),
+  CONSTRAINT `fk_Salaire_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `salaire`
+--
+
+LOCK TABLES `salaire` WRITE;
+/*!40000 ALTER TABLE `salaire` DISABLE KEYS */;
+INSERT INTO `salaire` VALUES (2,2022,1234,1234,1234,1234,1234,1234,1234,1234,1234,1234,1234,1234,12),(3,2023,1234,1234,1234,1234,1234,1234,1234,1234,1234,1234,1234,1234,12),(4,2021,1234,1234,1234,1234,1234,1234,1234,1234,1234,1234,1234,1234,12),(7,2020,1234,0,0,1234,1234,1234,0,0,0,1234,1234,1234,12),(8,2019,1234,0,0,1234,1234,1234,0,0,0,0,0,1234,12),(9,2018,1234,0,0,1234,1234,1234,0,0,0,0,0,1234,12);
+/*!40000 ALTER TABLE `salaire` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -65,12 +105,11 @@ CREATE TABLE `user` (
   `date_sortie` date DEFAULT NULL,
   `titre` varchar(45) NOT NULL,
   `salaire_base` float NOT NULL,
-  `salaire_reel` float DEFAULT NULL,
   `email` varchar(100) NOT NULL,
   `status` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -79,7 +118,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'BENKHEDIR','Maroua','0664403883','MASCULIN','1993-08-22','2002-10-24',NULL,'developpeur',3000,2800,'benkhedir.maroua@gmail.com','DEV'),(2,'dfdfs','dsfsdf','sdfdfsd','sdfdsfds','2022-01-23','2022-10-12','2022-10-15','hfdgfg',5000,3000,'hfgdfdf','ddf'),(3,'dsfdsfds','sdfdsf','fsdfsdf','dsfsdfsd','2022-01-23','2022-01-23','2022-01-23','fdsfdsf',1111,1111,'fdgfdgfdgfdg','ggg'),(5,'hadj hocine','fatma','0664403883','F','2023-02-01','2023-02-01','2023-02-01','developpeur',3000,2000,'email@gmail.com','dev'),(7,'hadj hocine','fatma','0664403883','F','2023-02-01','2023-02-01','2023-02-01','developpeur',3000,2000,'em@gmail.com','dev'),(9,'hadj hocine','fatma','0664403883','F','2023-02-01','2023-02-01','2023-02-01','developpeur',3000,2000,'e@gmail.com','dev'),(10,'test','test','0664403883','F','2023-02-01','2023-02-01','2023-02-01','manager',3000,2000,'test@gmail.com','manager');
+INSERT INTO `user` VALUES (12,'Victor','Hugo','011111111','H','2023-02-02','2023-02-02','2023-02-02','manager',3000,'hugo@gmail.com','Manager');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -92,4 +131,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-02  9:54:16
+-- Dump completed on 2023-02-03 15:34:58
